e853a6e6c4cb63ec28e695c280dbe945d8e5c95a
Joes Vos <hesseljoes@gmail.com>
2021-06-21 00:07:54 +0200

    removed some unnecessary lines

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -1,4 +1,3 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import {WeatherDataService, WeatherResponse} from "../weather-data.service";
 
@@ -6,21 +5,11 @@ import {WeatherDataService, WeatherResponse} from "../weather-data.service";
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: ['./weather-card.component.css'],
-  animations: [
-    trigger('bodyExpansion', [
-      state('collapsed, void', style({ height: '0px', visibility: 'hidden' })),
-      state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition('expanded <=> collapsed, void => collapsed',
-        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ])
-  ]
 })
 export class WeatherCardComponent implements OnInit {
   constructor(private weatherService : WeatherDataService) { }
   currentWeather: WeatherResponse;
   iconUrl: string;
-
-  state = 'collapsed';
 
   ngOnInit(): void {
     this.weatherService.getWeatherData().subscribe((res: WeatherResponse) => {
@@ -30,10 +19,6 @@ export class WeatherCardComponent implements OnInit {
 
 
   }
-  toggle(): void {
-    this.state = this.state === 'collapsed' ? 'expanded' : 'collapsed';
-  }
-
 }
 
 
