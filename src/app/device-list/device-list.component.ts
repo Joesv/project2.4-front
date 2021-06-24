5d68bff9413fdf22232c41735939badb4ed57016
Remerman <56107094+Remerman@users.noreply.github.com>
2021-06-24 17:23:32 +0200

    dummy devices changed

--- src/app/device-list/device-list.component.ts
+++ src/app/device-list/device-list.component.ts
@@ -4,6 +4,11 @@ import {
   LampDeviceResponse,
   LampDevicesResponse
 } from '../devices/lamp-device/lamp-device-data.service';
+import {
+  DummyDeviceDataService,
+  DummyDeviceResponse,
+  DummyDevicesResponse
+} from '../devices/dummy-device/dummy-device-data.service';
 
 @Component({
   selector: 'app-device-list',
@@ -12,13 +17,17 @@ import {
 })
 export class DeviceListComponent implements OnInit {
   lamps: Array<LampDeviceResponse> = [];
-
-  constructor(private lampDeviceDataService: LampDeviceDataService) { }
+  dummys: Array<DummyDeviceResponse> = [];
+  constructor(private lampDeviceDataService: LampDeviceDataService, private dummyDeviceDataService: DummyDeviceDataService) { }
 
   ngOnInit(): void {
     this.lampDeviceDataService.getLampDevices().subscribe((res: LampDevicesResponse) => {
       this.lamps = res.lamps;
       console.log(this.lamps);
+    });
+    this.dummyDeviceDataService.getDummyDevices().subscribe((res: DummyDevicesResponse) => {
+      this.dummys = res.dummys;
+      console.log(this.dummys);
     });
   }
 
