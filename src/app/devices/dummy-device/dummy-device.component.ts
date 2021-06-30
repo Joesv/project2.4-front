6577ecf25f295e948d16cf6eb5719c178ce9766d
Remerman <56107094+Remerman@users.noreply.github.com>
2021-06-30 10:37:47 +0200

    devices delete and save data

--- src/app/devices/dummy-device/dummy-device.component.ts
+++ src/app/devices/dummy-device/dummy-device.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DummyDeviceResponse} from './dummy-device-data.service';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-dummy-device',
@@ -9,9 +10,28 @@ import {DummyDeviceResponse} from './dummy-device-data.service';
 export class DummyDeviceComponent implements OnInit {
 
   @Input() dummy: DummyDeviceResponse;
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  changeStatus(value: number): void {
+    this.dummy.value = value;
+    const headers = {'content-type': 'application/json'};
+    this.http.put(`/api/dummy_device/${this.dummy.id}`, {status: value}, {headers, observe: 'response'})
+      .subscribe(resp => {
+        console.log('Updated Device!');
+        console.log(resp);
+      });
+  }
+
+  deleteDummy(): void {
+    const headers = {'content-type': 'application/json'};
+    this.http.delete(`/api/dummy_device/${this.dummy.id}`, {headers, observe: 'response'})
+      .subscribe(resp => {
+        console.log('Deleted Device!');
+        console.log(resp);
+      });
   }
 
 }
