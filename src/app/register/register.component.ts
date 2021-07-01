e2264744e3bb0b05621ed94d6b63768ebc4f72ca
DieEierVonSatan <47187431+DieEierVonSatan@users.noreply.github.com>
2021-07-01 11:09:49 +0200

    adding validator logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
-import {loginSucces} from "../login/login.component";
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {loginSucces} from '../login/login.component';
 
 @Component({
   selector: 'app-register',
@@ -11,54 +11,81 @@ import {loginSucces} from "../login/login.component";
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  username = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
+  name = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  password2 = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
   constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) {
+  }
+
+  ngOnInit(): void {
     this.form = this.fb.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      password2: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(6)],
+      password2: ['', Validators.required, Validators.minLength(6)],
       name: ['', Validators.required],
     });
   }
 
-  ngOnInit(): void {
-  }
-
   register() {
     const values = this.form.value;
-    console.log(values)
+    console.log(values);
     if (values.username && values.email && values.password && values.password2 && values.name && (values.password === values.password2)) {
-      const headers = {'content-type': 'application/json'}
-      const payload = {email: values.email, password: values.password, username: values.username}
-      const body = JSON.stringify(payload)
-      this.http.post("/api/user/register", body, {'headers': headers, observe: 'response'})
+      const headers = {'content-type': 'application/json'};
+      const payload = {email: values.email, password: values.password, username: values.username};
+      const body = JSON.stringify(payload);
+      this.http.post('/api/user/register', body, {headers: headers, observe: 'response'})
         .subscribe(resp => {
 
           switch (resp.status) {
             case 201:
-              const token: loginSucces = JSON.parse(JSON.stringify(resp.body))
-              localStorage.setItem('jwttoken', token.access_token)
-              const redirectTo = resp.headers.get("location")
-              this.router.navigate([redirectTo])
+              const token: loginSucces = JSON.parse(JSON.stringify(resp.body));
+              localStorage.setItem('jwttoken', token.access_token);
+              const redirectTo = resp.headers.get('location');
+              this.router.navigate([redirectTo]);
               break;
             case 303:
-              this.router.navigate([resp.headers.get("location")])
+              this.router.navigate([resp.headers.get('location')]);
               break;
           }
 
-        })
+        });
     }
   }
 
-  getErrorMessage() {
+  getErrorMessageEmail() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
-
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+  getErrorMessageUsername() {
+    if (this.username.hasError('required')) {
+      return 'You must enter a value';
+    }
+  }
+  getErrorMessageName() {
+    if (this.name.hasError('required')) {
+      return 'You must enter a value';
+    }
+  }
+  getErrorMessagePassword() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+    if (this.password.hasError('minlength')) {
+      return 'Minimum length is 8 characters';
+    }
+  }
+  getErrorMessagePassword2() {
+    if (this.password.hasError('required')) {
+      return 'You must enter a value';
+    }
+  }
+
 
 }
 
